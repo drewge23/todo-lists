c21f468e21e5f5d4a7db2bf98a0bbcb3163e1628
drewge23 <screwnut152@gmail.com>
2023-01-29 14:38:10 +0200

    Initial commit (firebase hosting configured)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAAg4yrkWjbY_mcyIBaL97U8eJApu8Lq0M",
+  authDomain: "todo-lists-db4d7.firebaseapp.com",
+  projectId: "todo-lists-db4d7",
+  storageBucket: "todo-lists-db4d7.appspot.com",
+  messagingSenderId: "622002488770",
+  appId: "1:622002488770:web:d36c9b9c9caff82ec5c25b",
+  measurementId: "G-5LVS8YGBD5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
